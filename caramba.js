4d468ca24ee0da03549e128ff71d9385704f0f61
Augusto Gonçalves Dias <augustodias2008@gmail.com>
2024-08-06 21:09:35 -0300

    sitefinal

---
+++ caramba.js
@@ -0,0 +1,8 @@
+let resposta = document.getElementById('resposta')
+
+function certo(){
+    let img = document.createElement("img");
+    img.src="mara.jpg"
+    document.body.appendChild(img);
+    
+}
